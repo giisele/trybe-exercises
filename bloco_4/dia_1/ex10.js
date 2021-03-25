0e2ff19cf90d125d26f3b6bc445423f08b452838
Gisele Costa <jikinha@gmail.com>
2021-03-25 15:07:00 -0300

    Adicionando os ultimos exercícios

--- bloco_4/dia_1/ex10.js
+++ bloco_4/dia_1/ex10.js
@@ -1,13 +1,10 @@
-let custoProduto, valorVenda;
+let valorCusto, valorVenda;
 
 
-
-
-// Escreva um programa que se inicie com dois valores em duas variáveis diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
-
-// Atente que, sobre o custo do produto, incide um imposto de 20%.
-// Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
-// O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
-//     valorCustoTotal = valorCusto + impostoSobreOCusto
-//     lucro = valorVenda - valorCustoTotal (lucro de um produto)
-
+if (valorCusto < 0 || valorVenda < 0) {
+  console.log('ERRO');
+} else {
+  let valorCustoTotal = 1.2 * valorCusto;
+  let lucro = valorVenda - valorCustoTotal;
+  let lucroTotal = lucro * 1000;
+}
