83355bc9b476495fbee678cfacdc3a1d037996f8
Gisele Costa <jikinha@gmail.com>
2021-03-25 19:01:26 -0300

    Atualização do exercicio 2

--- bloco_4/dia_2/ex2.js
+++ bloco_4/dia_2/ex2.js
@@ -1,6 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let i = 0, soma = 0; i < numbers.length(); i+=1){
+for (let i = 0, soma = 0; i < numbers.length; i+=1){
   soma += numbers[i];
 }
 
