956be08819e866206e42e139c996b5e04bd6a847
Gisele Costa <jikinha@gmail.com>
2021-03-26 14:50:29 -0300

    ex5: organizando a estrutura dos ifs

--- bloco_4/dia_1/ex5.js
+++ bloco_4/dia_1/ex5.js
@@ -1,6 +1,8 @@
 let ang1, ang2, ang3;
 
-if ((ang1 + ang2 + ang3) === 180 && ang1 > 0 && ang2 > 0 && ang3 > 0){
+if (ang1 < 0 || ang2 < 0 || ang3 < 0) {
+  console.log('ERRO');
+} else if ((ang1 + ang2 + ang3) === 180){
   console.log(true);
 } else {
   console.log(false);
