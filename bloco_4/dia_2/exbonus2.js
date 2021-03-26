ce95148081e4370440872f668bbfc1840d7678ca
Gisele Costa <jikinha@gmail.com>
2021-03-25 22:23:03 -0300

    Bubble sort decrescente num array e imprime

---
+++ bloco_4/dia_2/exbonus2.js
@@ -0,0 +1,13 @@
+let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27], position;
+
+for (let index = array.length - 1; index > 0; index -= 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (array[secondIndex] < array[secondIndex + 1]) {
+      position = array[secondIndex];
+      array[secondIndex] = array[secondIndex + 1];
+      array[secondIndex + 1] = position;
+    }
+  }
+}
+
+console.log(array);
