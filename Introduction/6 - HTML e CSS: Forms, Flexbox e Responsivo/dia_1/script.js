911b37d76ac904b8f0fc457039afb8dc9fc9dbdc
Gisele Costa  <jikinha@gmail.com>
2021-04-16 15:29:56 -0300

    Apenas o básico

---
+++ Introduction/6 - HTML e CSS: Forms, Flexbox e Responsivo/dia_1/script.js
@@ -0,0 +1,16 @@
+
+/* Estado - Todos os estados do Brasil
+Utilize estruturas de repetição via JavaScript para gerar os <option> */
+
+/* Data de início - Texto
+Verificar o formato da data dd/mm/aaaa .
+O dia deve ser > 0 e <= 31.
+O mês deve ser > 0 e <= 12.
+O ano não pode ser negativo.
+Caso alguma das condições seja inválida no momento do envio do formulário, exibir uma mensagem de erro contextualizada. */
+
+/* Chame uma função JavaScript que interrompa o fluxo automático do form utilizando o preventDefault() .
+Execute as validações que foram pedidas ao longo da montagem do formulário.
+Monte uma <div> com o consolidado dos dados que foram inseridos no formulário. */
+
+/* Monte uma <div> com o consolidado dos dados que foram inseridos no formulário */
