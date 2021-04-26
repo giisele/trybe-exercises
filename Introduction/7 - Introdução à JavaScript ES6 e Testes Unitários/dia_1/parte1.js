d8905452f022d0c020c53d89af617fa19cd7c687
Gisele Costa <jikinha@gmail.com>
2021-04-26 16:40:05 -0300

    Exercícios 1 e 2 da parte 1

---
+++ Introduction/7 - Introdução à JavaScript ES6 e Testes Unitários/dia_1/parte1.js
@@ -0,0 +1,26 @@
+// Exercício 1
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    let elseScope = `Não devo ser utilizada fora meu escopo (else)`;
+
+    console.log(elseScope);
+  }
+}
+
+testingScope(true);
+
+//Exercício 2
+const ex2 = () => {
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+  oddsAndEvens.sort((a, b) => a - b);
+
+  console.log(`Os números ${oddsAndEvens[0]},${oddsAndEvens[1]},${oddsAndEvens[2]},${oddsAndEvens[3]},${oddsAndEvens[4]},${oddsAndEvens[5]} se encontram ordenados de forma crescente!`);
+}
+
+ex2();
