9a648e72d17ea4d7d2066b5d745ace2c41d19106
Gisele Costa <jikinha@gmail.com>
2021-04-26 18:27:45 -0300

    Exercício 2 da parte 2

--- Introduction/7 - Introdução à JavaScript ES6 e Testes Unitários/dia_1/parte2.js
+++ Introduction/7 - Introdução à JavaScript ES6 e Testes Unitários/dia_1/parte2.js
@@ -2,3 +2,20 @@
 let fat = 1;
 const fatorial = (num) => {return fat = (num <= 1) ? 1 : num * fatorial(num - 1)}
 console.log(fatorial(5)); // 120
+
+// Exercício 2
+const longestWord = (string) => {
+  string = string.split(' ');
+  const wordLengthArr = string.map(word => word = word.length);
+  let biggest;
+
+  for (let index = 1; index <= wordLengthArr.length; index += 1) {
+    if (wordLengthArr[index] > wordLengthArr[index-1]) {
+      biggest = index;
+    }
+  }
+  
+  console.log(string[biggest]);
+}
+
+longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu'
