f69706bd015b1abb235a5b3c595bafeb68bb1401
Gisele Costa <jikinha@gmail.com>
2021-04-27 19:03:05 -0300

    Exercício 4 da parte 2

--- Introduction/7 - Introdução à JavaScript ES6 e Testes Unitários/dia_2/parte2.js
+++ Introduction/7 - Introdução à JavaScript ES6 e Testes Unitários/dia_2/parte2.js
@@ -30,9 +30,11 @@ listKeys(lesson1);
 const objectLength = (object) => console.log(Object.keys(object).length);
 objectLength(lesson1);
 
-/*
-4. Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+// Exercício 4
+const listValues = (object) => console.log(Object.values(object));
+listValues(lesson1);
 
+/*
 5. Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
 
 console.log(allLessons);
