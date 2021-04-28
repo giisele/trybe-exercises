81c30c8ef715c240be4ff581a34e6860b1a13116
Gisele Costa <jikinha@gmail.com>
2021-04-27 22:12:18 -0300

    Exercício 5 da parte 2

--- Introduction/7 - Introdução à JavaScript ES6 e Testes Unitários/dia_2/parte2.js
+++ Introduction/7 - Introdução à JavaScript ES6 e Testes Unitários/dia_2/parte2.js
@@ -34,30 +34,11 @@ objectLength(lesson1);
 const listValues = (object) => console.log(Object.values(object));
 listValues(lesson1);
 
-/*
-5. Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
-
+// Exercício 5
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
 console.log(allLessons);
 
-Output: {
-  lesson1:
-    { materia: 'Matemática',
-      numeroEstudantes: 20,
-      professor: 'Maria Clara',
-      turno: 'manhã' },
-  lesson2:
-    { materia: 'História',
-      numeroEstudantes: 20,
-      professor: 'Carlos',
-      turno: 'noite' },
-  lesson3:
-    { materia: 'Matemática',
-      numeroEstudantes: 10,
-      professor: 'Maria Clara',
-      turno: 'noite' }
-};
-
-
+/*
 6. Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
 
 7. Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
