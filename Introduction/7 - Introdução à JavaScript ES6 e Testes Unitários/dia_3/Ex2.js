381eea282603f6757289ab5eb2ffbdfd261efe94
Gisele Costa <jikinha@gmail.com>
2021-04-28 16:23:06 -0300

    Exercício 3

--- Introduction/7 - Introdução à JavaScript ES6 e Testes Unitários/dia_3/Ex2.js
+++ Introduction/7 - Introdução à JavaScript ES6 e Testes Unitários/dia_3/Ex2.js
@@ -14,7 +14,8 @@ assert.strictEqual(typeof myRemove, 'function');
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
 assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
 // Próximas três linhas fazem parte do gabarito, mas não entendi o porquê de chamar a função
+// Entendi ao fazer o exercício 3
 const myList = [5, 6, 7, 8];
 myRemove(myList, 5);
-assert.deepStrictEqual(myList, [5, 6, 7, 8]);
+assert.deepStrictEqual(myList, [5, 6, 7, 8]); // não dá erro pois retorna um novo array, diferente do que foi chamado
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
