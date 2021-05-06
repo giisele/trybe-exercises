59466a04bd0762139567bb891e13fc48bd535419
Gisele Costa <jikinha@gmail.com>
2021-05-06 15:11:15 -0300

    conteudo - rest

--- Introduction/8 - Higher Order Functions do JavaScript ES6/dia_5/conteúdo.js
+++ Introduction/8 - Higher Order Functions do JavaScript ES6/dia_5/conteúdo.js
@@ -1,4 +1,4 @@
-// spread
+// spread operator
 // Faça uma função chamada fruitSalad passando como parâmetro specialFruit e additionalItens , faça a função retornar uma lista única contendo todos os itens da nossa salada de frutas usando o spread .
 const specialFruit = ['banana', 'maçã', 'mamão'];
 
@@ -7,4 +7,16 @@ const additionalItens = ['leite condensado', 'granola', 'canela em pó'];
 const fruitSalad = (fruit, additional) => [...fruit, ...additional];
 
 console.log(fruitSalad(specialFruit, additionalItens));
+
+
+// parametro rest
+function quantosParams(...args) {
+  console.log('parâmetros:', args);
+  return `Você passou ${args.length} parâmetros para a função.`;
+}
+console.log(quantosParams(0, 1, 2)); // Você passou 3 parâmetros para a função.
+console.log(quantosParams('string', null, [1, 2, 3], { })); // Você passou 4 parâmetros para a função.
+
+const sum = (...args) => args.reduce((accumulator, current) => accumulator + current, 0);
+console.log(sum(4, 7, 8, 9, 60)); // 88
 
