f1c9067fad0828f38a5303a52583b2ab8697e165
Gisele Costa <jikinha@gmail.com>
2021-05-19 16:05:29 -0300

    resto do conteúdo e ex1

--- Introduction/10 - Testes automatizados com Jest/dia_2/conteudo.test.js
+++ Introduction/10 - Testes automatizados com Jest/dia_2/conteudo.test.js
@@ -69,3 +69,44 @@ test('Testando com async/await, testando o reject', async () => {
     expect(error).toEqual({ error: "Não possui esse tipo de animal." })
   }
 });
+
+// Matcher .resolves / .rejects
+describe('Testando promise - findAnimalsByType', () => {
+  describe('Quando o tipo do animal existe', () => {
+    test('Retorne a lista de animais', () => {
+      const listDogs = [
+        { name: 'Dorminhoco', age: 1, type: 'Dog' },
+        { name: 'Soneca', age: 2, type: 'Dog' },
+      ]
+      expect.assertions(1);
+      return expect(getListAnimals('Dog')).resolves.toEqual(listDogs)
+    });
+  });
+
+  describe('Quando o tipo de animal não existe', () => {
+    test('Retorna um erro', () => {
+      expect.assertions(1);
+      return expect(getListAnimals('Lion')).rejects.toEqual({ error: 'Não possui esse tipo de animal.' })
+    });
+  });
+});
+
+describe('Testando Async/Await - findAnimalsByType', () => {
+  describe('Quando o tipo do animal existe', () => {
+    test('Retorne a lista de animais', async () => {
+      const listDogs = [
+        { name: 'Dorminhoco', age: 1, type: 'Dog' },
+        { name: 'Soneca', age: 2, type: 'Dog' },
+      ]
+      expect.assertions(1);
+      await expect(getListAnimals('Dog')).resolves.toEqual(listDogs)
+    });
+  });
+
+  describe('Quando o tipo de animal não existe', () => {
+    test('Retorna um erro', async () => {
+      expect.assertions(1);
+      await expect(getListAnimals('Lion')).rejects.toEqual({ error: 'Não possui esse tipo de animal.' })
+    });
+  });
+});
