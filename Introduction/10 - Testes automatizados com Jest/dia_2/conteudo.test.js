b40aba088bea843271db1ce556548c9687653147
Gisele Costa <jikinha@gmail.com>
2021-05-19 17:57:59 -0300

    ex2 e ex3

--- Introduction/10 - Testes automatizados com Jest/dia_2/conteudo.test.js
+++ Introduction/10 - Testes automatizados com Jest/dia_2/conteudo.test.js
@@ -1,13 +1,13 @@
 // Callback
-const SumNumbers = (a, b, callback) => {
+const sumNumbers = (a, b, callback) => {
   setTimeout(() => {
     const result = a + b;
     callback(result);
   }, 500)
 }
 
-test('Testando SumNumbers, soma 5 mais 10', done => {
-  SumNumbers(5, 10, (result) => {
+test('Testando sumNumbers, soma 5 mais 10', done => {
+  sumNumbers(5, 10, (result) => {
     expect(result).toBe(15);
     done();
   });
