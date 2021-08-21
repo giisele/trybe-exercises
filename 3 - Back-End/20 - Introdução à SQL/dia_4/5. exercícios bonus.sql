168c4982e089a6550ba992ceb8aeba77695a5150
Gisele Costa <jikinha@gmail.com>
2021-08-20 21:32:44 -0300

    exercicios

---
+++ 3 - Back-End/20 - Introdução à SQL/dia_4/5. exercícios bonus.sql
@@ -0,0 +1,8 @@
+-- Exercício 8 : Altere a classificação da tabela BoxOffice para 9.0 de todos os filmes que lucraram mais de 400 milhões no mercado interno.
+
+
+-- Exercício 9 : Altere a classificação da tabela BoxOffice para 6.0 de todos os filmes que lucraram menos de 300 milhões no mercado internacional e mais de 200 milhões no mercado interno.
+
+
+-- Exercício 10 : Exclua da tabela Movies todos os filmes com menos de 100 minutos de duração.
+
