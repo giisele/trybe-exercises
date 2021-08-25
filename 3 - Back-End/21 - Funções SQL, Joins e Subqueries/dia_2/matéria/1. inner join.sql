afc8a2eab094cd2d2f5f81385e1d74207d2ec5c5
Gisele Costa <jikinha@gmail.com>
2021-08-25 19:36:03 -0300

    inner join

---
+++ 3 - Back-End/21 - Funções SQL, Joins e Subqueries/dia_2/matéria/1. inner join.sql
@@ -0,0 +1,6 @@
+-- * o INNER JOIN permite retornar todos os resultados em que a condição da cláusula ON for satisfeita. A sintaxe de um INNER JOIN é a seguinte:
+SELECT t1.coluna, t2.coluna
+FROM tabela1 AS t1
+INNER JOIN tabela2 AS t2
+ON t1.coluna_em_comum = t2.coluna_em_comum;
+
