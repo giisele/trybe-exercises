be5729c8fff749848bac956a83fb37da2bf8ebfc
Gisele Costa <jikinha@gmail.com>
2021-08-25 20:22:07 -0300

    union

---
+++ 3 - Back-End/21 - Funções SQL, Joins e Subqueries/dia_2/matéria/4. union.sql
@@ -0,0 +1,3 @@
+-- * O UNION nos permite unir os registros de uma tabela com outra, desde que usemos a mesma quantidade de colunas
+
+-- * O UNION remove os dados duplicados, enquanto o UNION ALL os mantém. Observe que, para usar o comando corretamente, a mesma quantidade de colunas deve ser utilizada
