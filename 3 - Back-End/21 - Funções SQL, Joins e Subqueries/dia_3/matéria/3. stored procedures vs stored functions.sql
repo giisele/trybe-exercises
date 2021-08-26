70ba91ff98e04d9d3b654482e18e433bfc90dda6
Gisele Costa <jikinha@gmail.com>
2021-08-26 20:29:32 -0300

    stored procedures vs stored functions

---
+++ 3 - Back-End/21 - Funções SQL, Joins e Subqueries/dia_3/matéria/3. stored procedures vs stored functions.sql
@@ -0,0 +1,6 @@
+-- ! Stored Functions VS Store Procedures
+-- * Stored Procedures podem ser chamadas através do comando CALL , e o retorno de um valor é opcional;
+-- * Já as Stored Functions são executadas com o comando SELECT e obrigatoriamente retornam algum valor;
+-- * A Stored Functions não pode ser utilizada para alterar o estado global da base dados. (Ex. Por meio das instruções INSERT , UPDATE e DELETE );
+-- * Stored Procedures permite alterar o estado global.
+-- * Stored Procedures permitem realizar o tratamento de excepções, via try/catch .
